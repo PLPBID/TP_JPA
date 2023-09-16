74eee01b16a209cb6baa33747beb861e1d657d06
PLPBID <47530262+PLPBID@users.noreply.github.com>
2023-09-16 18:02:43 -0300

    Add files via upload

---
+++ eje1/src/main/java/com/utn/eje1/Eje1Application.java
@@ -0,0 +1,13 @@
+package com.utn.eje1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Eje1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Eje1Application.class, args);
+	}
+
+}
---
+++ eje1/src/test/java/com/utn/eje1/Eje1ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.utn.eje1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Eje1ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
